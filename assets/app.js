31455417244d006d57447fcf2f9af350c61ca845
jaures kano <ruddyjaures@gmail.com>
2021-11-06 14:31:37 +0100

    [feat]: installation de react et mise en place du premier composants

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,12 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
 import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+import {createElement} from 'react'
+import {render} from 'react-dom'
+import Home from "./elements/home/Home";
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
-console.log('cathy')
+
+const domContainer = document.querySelector('#react');
+render(createElement(Home), domContainer);
+
---
+++ assets/elements/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/**
+ * Components Home
+ * @Author Jaures Kano <ruddyjaures@gmail.com>
+ */
+export default function Home(props) {
+    return (
+        <div>
+            react marche bien !!
+        </div>
+    );
+}
